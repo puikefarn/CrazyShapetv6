a8eee0d7ec88a3737c3377a90c4766fbd1e18c86
puikefarn <wochaoniba1234@gmail.com>
2021-05-31 12:19:39 +0800

    2021上机考v6_貝克帆 自己做

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/GameActivity.kt
@@ -0,0 +1,110 @@
+package tw.edu.pu.csim.tcyang.crazyshape
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_game.*
+import org.tensorflow.lite.support.image.TensorImage
+import tw.edu.pu.csim.tcyang.crazyshape.ml.Shapes
+
+class GameActivity : AppCompatActivity() {
+
+    var FlagShape:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        var intent = getIntent()
+        FlagShape = intent.getIntExtra("形状", 0)
+        when(FlagShape){
+            1->txvMsg.text = "请画出圆形"
+            2->txvMsg.text = "请画出方形"
+            3->txvMsg.text = "请画出星形"
+            4->txvMsg.text = "请画出三角形"
+
+        }
+        btnBack.isEnabled = false
+
+        btnBack.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                finish()
+            }
+        })
+
+        btn.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                handv.path.reset()
+                handv.invalidate()
+            }
+        })
+
+        handv.setOnTouchListener(object:View.OnTouchListener{
+            override fun onTouch(p0: View?, event: MotionEvent): Boolean {
+                var xPos = event.getX()
+                var yPos = event.getY()
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> handv.path.moveTo(xPos, yPos)
+                    MotionEvent.ACTION_MOVE -> handv.path.lineTo(xPos, yPos)
+                    MotionEvent.ACTION_UP -> {
+                        //將handv轉成Bitmap
+                        val b = Bitmap.createBitmap(handv.measuredWidth, handv.measuredHeight,
+                                Bitmap.Config.ARGB_8888)
+                        val c = Canvas(b)
+                        handv.draw(c)
+                        classifyDrawing(b)
+                    }
+                }
+                handv.invalidate()
+                return true
+            }
+        })
+    }
+
+
+    fun classifyDrawing(bitmap : Bitmap) {
+        val model = Shapes.newInstance(this)
+
+        // Creates inputs for reference.
+        val image = TensorImage.fromBitmap(bitmap)
+
+        // Runs model inference and gets result.
+        //val outputs = model.process(image)
+        //val probability = outputs.probabilityAsCategoryList
+
+        val outputs = model.process(image)
+                .probabilityAsCategoryList.apply {
+                    sortByDescending { it.score } // 排序，高匹配率優先
+                }.take(1)  //取最高的1個
+        var Result:String = ""
+        var FlagDraw:Int = 0
+        when (outputs[0].label) {
+            "circle" -> {Result = "圓形"
+                FlagDraw=1}
+            "square" -> {Result = "方形"
+                FlagDraw=2}
+            "star" -> {Result = "星形"
+                FlagDraw=3}
+            "triangle" -> {Result = "三角形"
+                FlagDraw=4}
+        }
+        //Result += ": " + String.format("%.1f%%", outputs[0].score * 100.0f)
+        Result = "您画的是"+ Result + ","
+        if(FlagShape==FlagDraw){
+            Result += "恭喜顺利过关！"
+            btnBack.isEnabled = true
+        }
+        else{
+            Result +="请再试试看喔！"
+        }
+
+        // Releases model resources if no longer used.
+        model.close()
+        Toast.makeText(this, Result, Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -0,0 +1,75 @@
+package tw.edu.pu.csim.tcyang.crazyshape
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import kotlinx.android.synthetic.main.activity_main.*
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
+
+class MainActivity : AppCompatActivity() {
+
+    var Flag:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(baseContext, "作者：貝克帆", Toast.LENGTH_LONG).show()
+        GlideApp.with(this)
+                .load(R.drawable.cover)
+                .override(800, 600)
+                .into(imgTitle)
+
+        RndShape()
+
+
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java).apply{
+                    putExtra("形状",Flag)
+                }
+
+                //startActivity(intent)
+                startActivityForResult(intent, 99)
+                return true
+            }
+        })
+
+
+        imgNext.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?){
+            RndShape()
+            }
+        })
+    }
+
+    fun RndShape(){
+        Flag=(1..4).random()
+        when(Flag){
+            1->imgNext.setImageResource(R.drawable.circle)
+            2->imgNext.setImageResource(R.drawable.square)
+            3->imgNext.setImageResource(R.drawable.star)
+            4->imgNext.setImageResource(R.drawable.triangle)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 99){
+            intent = Intent(this@MainActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+
+}
